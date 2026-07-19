3260cf030661b2184828435905cf1d3ebfbbc69f
treywwre-rgb <treyw.wre@gmail.com>
2026-07-18 22:50:39 -0500

    Working on error

---
+++ youtube-clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+import Sidebar from '../../Components/Navbar/Sidebar/Sidebar'
+
+
+const Home = () => {
+  return (
+    <> 
+      <Sidebar />
+    </>
+  )
+}
+
+export default Home
