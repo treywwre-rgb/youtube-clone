d3b86f2144f18d9c57329c2086188ca2be7cc84e
treywwre-rgb <treyw.wre@gmail.com>
2026-07-24 17:57:11 -0500

    fixing errors

--- youtube-clone/src/Components/Navbar/Feed/Feed.jsx
+++ youtube-clone/src/Components/Navbar/Feed/Feed.jsx
@@ -16,7 +16,7 @@ const Feed = ({category}) => {
     const [data,setData] = useState([]);
 
     const fetchData = async () =>{
-        const videoList_url = ` https://www.googleapis.com/youtube/v3/videos&videoCategoryId=${category}&key=${API_KEY}`
+       const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`
         await fetch(videoList_url).then(response=>response.json()).then(data=>setData(data.items))
     }
 
